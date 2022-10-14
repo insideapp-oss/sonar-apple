73d023a8556814543946a6e378f4ab15e8285bfe
Gaël Foppolo <gael.foppolo@insideapp.fr>
2022-10-14 15:00:15 +0200

    Fix runtime issue in Sonar 7.X

--- objc-lang/src/main/java/fr/insideapp/sonarqube/objc/issues/mobsfscan/MobSFScanObjectiveCSensor.java
+++ objc-lang/src/main/java/fr/insideapp/sonarqube/objc/issues/mobsfscan/MobSFScanObjectiveCSensor.java
@@ -26,7 +26,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public final class MobSFScanObjectiveCSensor extends MobSFScanSensor {
 
-    protected MobSFScanObjectiveCSensor() {
+    public MobSFScanObjectiveCSensor() {
         super(new MobSFScanObjectiveCRulesDefinition());
     }
 
